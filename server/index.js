3310a15486504cfddf365663a8cf2a548aef488b
voit899 <voit899@gmail.com>
2022-08-18 19:32:06 +0300

    nIndex

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
-const express = require ("express")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const cartRouter = require('./routers/cartRouter');
+const userRouter = require('./routers/usersRouter');
+const paymentsRouter = require('./routers/paymentsRouter');
+const productRouter = require('./routers/productRouter');
+const authRouter = require('./routers/authRouter');
 
 const app = express();
-dotenv.config({path: '.env'});
-const port = process.env.PORT;
-const url = process.env.URL;
 
-app.listen(port, "localhost", () => {
-    console.log(`listening on ${port}`)
-})
+app.use(express.json({ limit: '10kb' }));
+
+dotenv.config({ path: './.env' });
+
+app.use('/cart', cartRouter);
+app.use('/users', userRouter);
+app.use('/payments', paymentsRouter);
+app.use('/products', productRouter);
+app.use('/auth', authRouter);
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(_ => console.log('connected to mongo'));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, 'localhost', () => {
+    console.log(`listening on localhost:${port}`);
+});
