f227ff19901aac624c8d4472ae2bf90fe8091347
voit899 <voit899@gmail.com>
2022-08-18 19:29:01 +0300

    uuid

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,1 +1,10 @@
 const mongose = require('mongose')
+const crypto = require("crypto")
+const uuid = crypto.randomUUID()
+
+const user = [{
+    id: 1,
+    name: '',
+    email: '',
+    password: ''
+}]
